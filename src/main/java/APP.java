a96bb909bf5896b765d0c016b2c8fdd5f348c964
Joshua Ashby <joshuaashby16@gmail.com>
2021-06-05 15:09:27 -0400

    Initial commit

---
+++ src/main/java/APP.java
@@ -0,0 +1,25 @@
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class APP {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("What is your current age?");
+        Scanner sc = new Scanner(System.in);
+        String CurAge = sc.nextLine();
+        System.out.println("At what age would you like to retire?");
+        String RetAge = sc.nextLine();
+        int CurAgeInt = Integer.parseInt(CurAge);
+        int RetAgeInt = Integer.parseInt(RetAge);
+        int YearsToRet = RetAgeInt - CurAgeInt;
+        int CurrentYear = Calendar.getInstance().get(Calendar.YEAR);
+        int HowLong = CurrentYear + YearsToRet;
+
+        System.out.println("You have "+ YearsToRet + " years left until you can retire.");
+        System.out.println("It's "+ CurrentYear + ", so you can retire in " + HowLong + ".");
+
+
+    }
+}
